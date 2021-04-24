69a19821f8d25eecf20f92e8b3517fbb484628b8
Mert Akpınar <mert.akpinar@hotmail.com>
2021-04-25 00:06:09 +0300

    Google Login

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,18 +1,22 @@
 import jwt from 'jsonwebtoken'
-
 const auth = async (req, res, next) => {
   try {
     const accessToken = req.headers.authorization.split(' ')[1]
 
-    jwt.verify(
-      accessToken,
-      process.env.ACCESS_TOKEN_SECRET,
-      (err, decodedAccessToken) => {
-        if (err) return res.status(403).json(err.message)
-        req.creatorId = decodedAccessToken?.id
-        next()
-      }
-    )
+    const isGoogleLogin = accessToken.length > 500
+
+    let decodedToken
+
+    if (accessToken && !isGoogleLogin) {
+      decodedToken = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET)
+      req.creatorId = decodedToken?.id
+    } else {
+      decodedToken = jwt.decode(accessToken)
+
+      req.creatorId = decodedToken?.sub
+    }
+
+    next()
   } catch (error) {
     console.log(error)
   }
